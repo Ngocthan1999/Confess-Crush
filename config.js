b6f346426bf04bb4abcc7343e7c384be16419bb1
Ngocthan1999 <101113521+Ngocthan1999@users.noreply.github.com>
2022-06-04 20:42:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "Chúc Mừng Ngày 8/3",
-    introTitle: 'Trần Nhật Minh Thư',
-    introDesc: `Hôm này Bé biết ngày gì hong . Nay là ngày 8 /3 nè . cho Bé xem này nè `,
+    titleWeb: "Chúc Mừng Ngày 6/4",
+    introTitle: 'Trần Ngoc Bảo Trân',
+    introDesc: `Hôm này Bé biết ngày gì hong . Nay là ngày 6/4 nè . cho Bé xem này nè `,
     btnIntro: 'Bấm Vào đây bé nha',
     title: 'Phải chăng anh đã yêu Bé ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Bé nói khi nào Bé về việt Nam bé dẫn a đi chơi phải hong :))',
+    desc: 'Bé nói , bé dẫn a đi chơi phải hong :))',
     btnYes: 'Đúng ạ :3',
     btnNo: 'Không nha :3',
     question: 'Anh biết Bé sẻ chọn đồng ý mà kaka . bé có lời yêu thương nào muốn gửi đến anh yêu hong <3',
     btnReply: 'Gửi cho anh nè <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết Bé nói dậy mà🥰. Yêu bé nhiều nhiều 😘😘',
-    messDesc: 'Chốt lại chúc bé 8/3 dui dẻ nghen, dìa sài gòn a đón đi chơi nha.',
+    messDesc: 'Chốt lại chúc bé 6/4 dui dẻ nghen',
     btnAccept: 'Okiiiii ạ <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
